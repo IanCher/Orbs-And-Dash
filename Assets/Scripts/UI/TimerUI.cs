1360ef4749dca09c425756095967b531dd8366e0
Herleen <herleen@floreovr.com>
2025-08-09 14:07:29 -0700

    Add TimerUI script and prefab

---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+public class TimerUI : MonoBehaviour
+{
+    private TMP_Text timerUIText;
+    private float time;
+    void Awake()
+    {
+        timerUIText = GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+        timerUIText.text = timeSpan.ToString(@"mm\:ss\:ff");
+    }
+}
