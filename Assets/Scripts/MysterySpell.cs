188cfa505a56a60fea9075ddcae4bf7f5d223fb1
IanCher <IanCher@users.noreply.github.com>
2025-09-23 20:50:49 +0200

    Add visualisation for mystery spell - alternating between potion and shield asset

--- Assets/Scripts/MysterySpell.cs
+++ Assets/Scripts/MysterySpell.cs
@@ -4,6 +4,10 @@ using System;
 public class MysterySpell : MonoBehaviour, ICollideable
 {
     public static event Action<MysterySpellEffect> OnCollideWithMysterySpell;
+    [SerializeField] GameObject[] assetsForVisualisation;
+    [SerializeField] float alternatingTimeBetweenAssets = 0.5f;
+    private float timeSinceLastAssetChange = 0;
+    private int currentActiveAssetIdx = 0;
 
     [Header("MysterySpell Weights normalized to 100%")]
     [Min(0f)] public float takeDamageChance = 50f;
@@ -14,6 +18,33 @@ public class MysterySpell : MonoBehaviour, ICollideable
     public ShieldData shieldData;
 
     private bool wasHitByPlayer = false;
+
+    void Start()
+    {
+        foreach (GameObject asset in assetsForVisualisation)
+        {
+            asset.SetActive(false);
+        }
+        assetsForVisualisation[0].SetActive(true);
+    }
+
+    void Update()
+    {
+        if (timeSinceLastAssetChange > alternatingTimeBetweenAssets)
+        {
+            assetsForVisualisation[currentActiveAssetIdx].SetActive(false);
+
+            currentActiveAssetIdx++;
+            if (currentActiveAssetIdx >= assetsForVisualisation.Length) currentActiveAssetIdx = 0;
+
+            assetsForVisualisation[currentActiveAssetIdx].SetActive(true);
+            timeSinceLastAssetChange = 0;
+        }
+        else
+        {
+            timeSinceLastAssetChange += Time.deltaTime;
+        }
+    }
 
     public void HandlePlayerCollision()
     {
