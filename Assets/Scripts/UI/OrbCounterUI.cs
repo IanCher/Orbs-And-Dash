51ccffcf08db63bfe128b295f0ac007020816d76
IanCher <IanCher@users.noreply.github.com>
2025-10-25 17:52:53 +0200

    Bugfix - High orb counter ui is now being updated

--- Assets/Scripts/UI/OrbCounterUI.cs
+++ Assets/Scripts/UI/OrbCounterUI.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 public class OrbCounterUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI lowOrbCountText;
-    [SerializeField] private TextMeshProUGUI midOrbCountText;
-    // [SerializeField] private TextMeshProUGUI highOrbCountText;
+    // [SerializeField] private TextMeshProUGUI midOrbCountText;
+    [SerializeField] private TextMeshProUGUI highOrbCountText;
     // [SerializeField] private TextMeshProUGUI totalOrbCountText;
 
 
     private void Start()
     {
         lowOrbCountText.text = "x0";
-        midOrbCountText.text = "x0";
-        // highOrbCountText.text = "x0";
+        // midOrbCountText.text = "x0";
+        highOrbCountText.text = "x0";
         OrbCounterManager.OnSendOrbCountVisualUpdateRequest += OrbCounterManager_OnSendOrbCountVisualUpdateRequest;
     }
 
@@ -25,8 +25,8 @@ public class OrbCounterUI : MonoBehaviour
     private void OrbCounterManager_OnSendOrbCountVisualUpdateRequest(int arg1, int arg2, int arg3, int arg4)
     {
         lowOrbCountText.text = "x" + arg1.ToString();
-        midOrbCountText.text = "x" + arg2.ToString();
-        // highOrbCountText.text = "x" + arg3.ToString();
+        // midOrbCountText.text = "x" + arg2.ToString();
+        highOrbCountText.text = "x" + arg3.ToString();
         // totalOrbCountText.text = "x" + arg4.ToString();
     }
 }
