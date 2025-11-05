8ad264cc60174b2fc31eb1061a8c248a934beb82
IanCher <IanCher@users.noreply.github.com>
2025-11-05 22:11:55 +0100

    Player blinks when hit and it temporarily protected

--- Assets/Scripts/Player/PlayerCollisionDetection.cs
+++ Assets/Scripts/Player/PlayerCollisionDetection.cs
@@ -1,15 +1,28 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerCollisionDetection : MonoBehaviour
 {
     [SerializeField] GameObject loseOrbsAnimator;
+
+    Animator animator;
+
+    [SerializeField] float timeProtectedAfterBeingHit = 1.5f;
+    public bool isTemporarilyProtected = false;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if(other.TryGetComponent<ICollideable>(out ICollideable collidable))
+        if (other.TryGetComponent<ICollideable>(out ICollideable collidable))
         {
-
+            if (isTemporarilyProtected && IsOtherObstacle(other)) return;
             if (other.GetComponent<PotionBomb>() || other.GetComponent<StaticWall>())
             {
                 var loseOrbAnim = Instantiate(
@@ -17,10 +30,28 @@ public class PlayerCollisionDetection : MonoBehaviour
                     transform.position,
                     transform.rotation
                 );
-                Destroy(loseOrbAnim.gameObject, 3f);
-            } 
-             
+                Destroy(loseOrbAnim, 3f);
+                StartCoroutine(SetProtectionForLimitedTime());
+                animator.SetTrigger("GotHit");
+            }
+
             collidable.HandlePlayerCollision();
         }
+    }
+
+    bool IsOtherObstacle(Collider other)
+    {
+        if (other.GetComponent<PotionBomb>()) return true;
+        if (other.GetComponent<StaticWall>()) return true;
+        if (other.GetComponent<ParalysingSpell>()) return true;
+
+        return false;
+    }
+    
+    IEnumerator SetProtectionForLimitedTime()
+    {
+        isTemporarilyProtected = true;
+        yield return new WaitForSeconds(timeProtectedAfterBeingHit);
+        isTemporarilyProtected = false;
     }
 }
