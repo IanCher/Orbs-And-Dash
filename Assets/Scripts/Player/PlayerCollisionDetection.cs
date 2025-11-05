e07883a5fbbe8aa4ae7dd75dccd9783be5f27a64
IanCher <IanCher@users.noreply.github.com>
2025-11-05 21:47:52 +0100

    Play a mini animation showing that the player lose orbs when hit potion bomb or wall

--- Assets/Scripts/Player/PlayerCollisionDetection.cs
+++ Assets/Scripts/Player/PlayerCollisionDetection.cs
@@ -1,12 +1,25 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCollisionDetection : MonoBehaviour
 {
+    [SerializeField] GameObject loseOrbsAnimator;
     void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent<ICollideable>(out ICollideable collidable))
         {
+
+            if (other.GetComponent<PotionBomb>() || other.GetComponent<StaticWall>())
+            {
+                var loseOrbAnim = Instantiate(
+                    loseOrbsAnimator,
+                    transform.position,
+                    transform.rotation
+                );
+                Destroy(loseOrbAnim.gameObject, 3f);
+            } 
+             
             collidable.HandlePlayerCollision();
         }
     }
