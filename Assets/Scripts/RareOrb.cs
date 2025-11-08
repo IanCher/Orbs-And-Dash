4a0e2ae3d1c62ecbfadc6f9c20578e46786faf89
IanCher <IanCher@users.noreply.github.com>
2025-11-08 14:48:05 +0100

    Fix - Collision with rare orb is only detected once

--- Assets/Scripts/RareOrb.cs
+++ Assets/Scripts/RareOrb.cs
@@ -4,9 +4,13 @@ public class RareOrb : MonoBehaviour , ICollideable
 {
 
     public string id;
+    private bool wasCollected = false;
 
     public void HandlePlayerCollision()
     {
+        if (wasCollected) return;
+
+        wasCollected = true;
         PlayerData.RareOrbsTrack += id;
         PlayerData.RareOrbs++;
         gameObject.SetActive(false);
